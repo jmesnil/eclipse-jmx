c5745c092a27cedb55636cb2ef83ee8f30b7dd8e
Jeff Mesnil <jmesnil@gmail.com>
2007-02-07 13:12:29 +0000

    * tagged build I200702071405

---
+++ net.jmesnil.jmx.core/src/net/jmesnil/jmx/core/JMXCoreActivator.java
@@ -0,0 +1,76 @@
+/**
+ * Eclipse JMX Console
+ * Copyright (C) 2006 Jeff Mesnil
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package net.jmesnil.jmx.core;
+
+import java.io.IOException;
+
+import javax.management.MBeanServerConnection;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+import net.jmesnil.jmx.resources.MBeanServerConnectionWrapper;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class JMXCoreActivator extends Plugin {
+
+    // The plug-in ID
+    public static final String PLUGIN_ID = "net.jmesnil.jmx.core"; //$NON-NLS-1$
+
+    // The shared instance
+    private static JMXCoreActivator plugin;
+
+    public JMXCoreActivator() {
+    }
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static JMXCoreActivator getDefault() {
+        return plugin;
+    }
+
+    public MBeanServerConnectionWrapper connect(String url) throws IOException {
+        Assert.isNotNull(url);
+        JMXServiceURL jmxurl = new JMXServiceURL(url);
+        JMXConnector connector = JMXConnectorFactory.connect(jmxurl);
+        MBeanServerConnection mbsc = connector.getMBeanServerConnection();
+        MBeanServerConnectionWrapper wrapper = new MBeanServerConnectionWrapper(
+                mbsc);
+        return wrapper;
+    }
+}
