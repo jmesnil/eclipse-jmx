72ab99b0b44813dc004d3055b611a1a1ac78c75c
Jeff Mesnil <jmesnil@gmail.com>
2007-02-21 19:43:08 +0000

    * added ruby scripts to automate tagging of all projects composing eclipse-jmx

---
+++ net.jmesnil.jmx.releng/scripts/svn-tag-eclipse-jmx.rb
@@ -0,0 +1,35 @@
+require "fileutils"
+
+# tag argument is mandatory.
+# it is expected to follow the format IHHHHMMDDhhmm (e.g. I200702071405)
+tag = ARGV[0]
+exit unless tag
+
+projects = [
+  "net.jmesnil.jmx.core",
+  "net.jmesnil.jmx.core.test",
+  "net.jmesnil.jmx.feature", 
+  "net.jmesnil.jmx.resources",
+  "net.jmesnil.jmx.ui",
+  "net.jmesnil.jmx.ui.test",
+  "net.jmesnil.jmx.ui.test.interactive"]
+
+repository = "https://eclipse-jmx.googlecode.com/svn"
+trunk_repository = "#{repository}/trunk"
+tag_repository = "#{repository}/tags/#{tag}"
+
+username = "jmesnil"
+
+mkdir_tag_cmd = "svn mkdir --username=#{username} -m '* creating tag #{tag}' #{tag_repository}"
+puts mkdir_tag_cmd
+system mkdir_tag_cmd
+
+workspace_dir = File.dirname(__FILE__) + "/../../"
+FileUtils.cd workspace_dir
+
+tag_msg = "'* tagged as #{tag}'"
+projects.each do | project |
+  tag_cmd = "svn copy -m #{tag_msg} #{project} #{tag_repository}/#{project}"
+  puts tag_cmd
+  system tag_cmd
+end
