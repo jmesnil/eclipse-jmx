a9b3dae86cdda9d3ee43a8cf7f3aa6a130a12a89
Jeff Mesnil <jmesnil@gmail.com>
2007-03-01 20:45:53 +0000

    * tagged as I200703012200

---
+++ net.jmesnil.jmx.ui/src/net/jmesnil/jmx/ui/internal/editors/NotificationsPage.java
@@ -0,0 +1,44 @@
+/**
+ * Eclipse JMX Console
+ * Copyright (C) 2007 Jeff Mesnil
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package net.jmesnil.jmx.ui.internal.editors;
+
+import net.jmesnil.jmx.resources.MBeanInfoWrapper;
+import net.jmesnil.jmx.ui.internal.Messages;
+
+import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.editor.FormEditor;
+import org.eclipse.ui.forms.editor.FormPage;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+
+public class NotificationsPage extends FormPage {
+
+    static final String ID = "notifications"; //$NON-NLS-1$
+
+    private MBeanInfoWrapper wrapper;
+
+    public NotificationsPage(FormEditor editor) {
+        super(editor, ID, Messages.NotificationsPage_title);
+        MBeanEditorInput input = (MBeanEditorInput) editor.getEditorInput();
+        this.wrapper = input.getWrapper();
+    }
+
+    protected void createFormContent(IManagedForm managedForm) {
+        ScrolledForm form = managedForm.getForm();
+        form.setText(wrapper.getObjectName().getCanonicalName());
+        form.getForm().setSeparatorVisible(true);
+    }
+}
---
+++ net.jmesnil.jmx.ui/src/net/jmesnil/jmx/ui/internal/tree/NodeUtils.java
@@ -0,0 +1,43 @@
+/**
+ * Eclipse JMX Console
+ * Copyright (C) 2007 Jeff Mesnil
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * 
+ */
+package net.jmesnil.jmx.ui.internal.tree;
+
+import javax.management.ObjectName;
+
+public class NodeUtils {
+
+    public static ObjectNameNode findObjectNameNode(Node node,
+            ObjectName objectName) {
+        if (node instanceof ObjectNameNode) {
+            ObjectNameNode onNode = (ObjectNameNode) node;
+            if (onNode.getObjectName().equals(objectName)) {
+                return onNode;
+            }
+        }
+        Node[] children = node.getChildren();
+        for (int i = 0; i < children.length; i++) {
+            Node child = children[i];
+            Node found = findObjectNameNode(child, objectName);
+            if (found != null) {
+                return (ObjectNameNode) found;
+            }
+        }
+        return null;
+    }
+
+}
