258257229fa0df94f997bed5b8d72790858965fe
Rob Stryker <rawblem@gmail.com>
2008-09-23 19:42:04 +0000

    header

--- net.jmesnil.jmx.ui/src/net/jmesnil/jmx/ui/internal/actions/DeleteConnectionAction.java
+++ net.jmesnil.jmx.ui/src/net/jmesnil/jmx/ui/internal/actions/DeleteConnectionAction.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Jeff Mesnil
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package net.jmesnil.jmx.ui.internal.actions;
 
 import net.jmesnil.jmx.core.IConnectionWrapper;
@@ -10,6 +17,11 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 
+/**
+ * The action to delete a connection
+ * @author "Rob Stryker"<rob.stryker@redhat.com>
+ *
+ */
 public class DeleteConnectionAction extends Action {
 	private IConnectionWrapper connection;
 	public DeleteConnectionAction(IConnectionWrapper wrapper) {
